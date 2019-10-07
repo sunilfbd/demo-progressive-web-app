ac0d1e885f83ab659155515505f1fd7f5c0615c0
Sunil Ahuja <sunil.ahuja@akqa.com>
2019-10-07 14:18:31 +0530

    configured webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,19 @@
 const webpack = require('webpack');
 const path = require('path');
+const glob = require('glob');
+const devServer = require('webpack-dev-server');
 
 
 var config = {
     mode: 'development',
     watch: true,
-    entry: {
-
-    },
+    entry: glob.sync('./src/**/*.css'),
     output: {
         path: path.resolve(__dirname, './src/scripts'),
         filename: 'bundle.js'
     },
     devServer: {
-        contentBase: path.resolve(__dirname, './src'),
-        compress: true,
-        port: 9000
+        contentBase: path.resolve(__dirname, './src')
     }
 };
 
