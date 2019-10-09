3782fbb215fc327db132d6b05e83140d23f5b7a2
Sunil Ahuja <sunil.ahuja@akqa.com>
2019-10-09 09:09:20 +0530

    changed files location

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,41 @@ const webpack = require('webpack');
 const path = require('path');
 const glob = require('glob');
 const devServer = require('webpack-dev-server');
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 var config = {
     mode: 'development',
     watch: true,
-    entry: glob.sync('./src/**/*.css'),
+    entry: glob.sync('./src/scripts/components/*.js'),
     output: {
-        path: path.resolve(__dirname, './src/scripts'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, './dist/styles'),
+        filename: 'bundle.css'
     },
     devServer: {
         contentBase: path.resolve(__dirname, './src')
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(css|sass|scss)$/,
+                use:[
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    'css-loader',
+                    'sass-loader',
+                    'style-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: './dist/bundle.css'
+        }),
+    ],
 };
 
 module.exports = config;
